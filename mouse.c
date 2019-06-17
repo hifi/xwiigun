a24aeaa058ae828f90ab2576519c84d154c9a025
7777777adow <7oxicshadow@googlemail.com>
2019-06-17 20:09:53 +0100

    Fix missing semicolon (Add Comments, Clean up tabs, Remove goto, minor changes to arg handler)

--- mouse.c
+++ mouse.c
@@ -85,7 +85,7 @@ int main(int argc, char **argv)
     /* Try to open a uinput device and respond accordingly */
     if ((fd = open("/dev/uinput", O_WRONLY | O_NONBLOCK)) < 0) {
         perror("open");
-        printf("Try running the application using sudo so it has access to /dev/uinput\n")
+        printf("Try running the application using sudo so it has access to /dev/uinput\n");
         return 1;
     }
 
